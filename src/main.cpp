1ac488e05c4fcd7592adf97e787292f827c7743b
Ethan Roth <rothe2@s2.rpn.ch>
2026-07-26 01:10:30 +0200

    feat: Tested first Fourier serie, works perfectly

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,60 @@
 
 int main()
 {
+    // Window size
     const int SCREENWIDTH = 1960;
     const int SCREENHEIGHT = 1080;
 
+    // Init window
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "Fourier series - Epicycles");
 
+    // Vars
     Vector2 center = {SCREENWIDTH / 2, SCREENHEIGHT / 2};
-    Epicycle firstCircle(center, 100, 0, 2);
-    Epicycle secondCircle(firstCircle.GetTipPosition(), 50, 10, 4);
-    Epicycle thirdCircle(secondCircle.GetTipPosition(), 50, 10, 6);
 
+    // Path and epicycles
     std::vector<Vector2> path;
+    std::vector<Epicycle> epicycles;
+
+    // Fill epicycles vector
+    int circlesNumber = 5;
+
+    for (int i = 0; i < circlesNumber; i++)
+    {
+        int n = i * 2 + 1;
+
+        float radius = 200.0f * (4.0f / (PI * n));
+
+        float speed = 1.0f * n;
+
+        epicycles.push_back(Epicycle(center, radius, 0, speed));
+    }
 
     while (!WindowShouldClose())
     {
         // Update everything in time
         float deltaTime = GetFrameTime();
-        firstCircle.Update(deltaTime);
-        secondCircle.SetCenter(firstCircle.GetTipPosition());
-        secondCircle.Update(deltaTime);
-        thirdCircle.SetCenter(secondCircle.GetTipPosition());
-        thirdCircle.Update(deltaTime);
+        for (int i = 0; i < epicycles.size(); i++)
+        {
+            if (i != 0)
+                epicycles[i].SetCenter(epicycles[i - 1].GetTipPosition());
+            else
+                epicycles[i].SetCenter(center);
 
-        path.push_back(thirdCircle.GetTipPosition());
+            epicycles[i].Update(deltaTime);
+        }
+
+        // Fill path points list
+        path.push_back(epicycles.back().GetTipPosition());
 
         // Start of drawing
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Draw circle
-        firstCircle.Draw();
-        secondCircle.Draw();
-        thirdCircle.Draw();
+        // Draw circles
+        for (int i = 0; i < epicycles.size(); i++)
+            epicycles[i].Draw();
+
+        // Draw path
         DrawLineStrip(path.data(), (int)path.size(), DARKBLUE);
 
         // End of drawing
